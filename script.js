8ce76a5dac84abf941dc02776f3aee385bdc13a6
FRJ74 <frankr74@gmail.com>
2024-05-23 23:01:05 -0600

    change the while condition to use the less than operator, instead of the less than or equal operator.

--- script.js
+++ script.js
@@ -4,28 +4,34 @@ const character = "#";
 const count = 8;
 const rows = [];
 
+function padRow(rowNumber, rowCount) {
+    return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+};
+
 // TODO: Use a different kind of loop.
 
 /*for (let i = 1; i <= count; i ++) {
     rows.push(padRow(i, count));
 };*/
 
-let result = "";
 let done = 0;
 
-while (rows.length <= count) {
-  rows.push(padRow(done, count));
+while (rows.length < count) {
+    done++;
+  rows.push(padRow(rows.length + 1, count));
   
 };
+
+let result = "";
 
 for (const row of rows) {
     result = result + "\n" + row;
     
 };
 
-function padRow(rowNumber, rowCount) {
-    return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
-};
+console.log(result);
+
+
 
 
 
