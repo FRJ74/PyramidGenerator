cd5ca410dc8b2acc4e766504ed03e5f861b5627f
FRJ74 <frankr74@gmail.com>
2024-05-25 13:37:25 -0600

    Remove the numbers array, and the method calls and log calls

--- script.js
+++ script.js
@@ -24,9 +24,7 @@ function padRow(rowNumber, rowCount) {
     rows.push(padRow(i, count));
 }*/
 
-const numbers = [1,2,3];
-const unshifted = numbers.unshift(5);
-const shifted = numbers.shift();
+
 
 let result = "";
 
